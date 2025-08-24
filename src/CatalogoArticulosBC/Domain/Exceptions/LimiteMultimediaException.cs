7cba4833986ee0e0227afbc5efcdd069cc6c980e
Antoniohuaman <a.huaman@contasiscorp.com>
2025-08-24 18:11:13 -0500

    feat: Remove obsolete multimedia interfaces and use cases; update exception classes to inherit from DomainException for better error handling

--- src/CatalogoArticulosBC/Domain/Exceptions/LimiteMultimediaException.cs
+++ src/CatalogoArticulosBC/Domain/Exceptions/LimiteMultimediaException.cs
@@ -1,9 +1,15 @@
 using System;
+using SharedKernel.Exceptions;
 
 namespace CatalogoArticulosBC.Domain.Exceptions
 {
-    public class LimiteMultimediaException : Exception
+    public class LimiteMultimediaException : DomainException
     {
-        public LimiteMultimediaException() : base("El producto ya tiene el máximo de archivos permitidos (5).") { }
+        public LimiteMultimediaException()
+            : base(
+                message: "El producto ya tiene el máximo de archivos permitidos (5).",
+                code: "LimiteMultimedia",
+                innerException: null)
+        { }
     }
 }
--- src/CatalogoArticulosBC/Domain/Exceptions/MultimediaInvalidaException.cs
+++ src/CatalogoArticulosBC/Domain/Exceptions/MultimediaInvalidaException.cs
@@ -1,9 +1,15 @@
 using System;
+using SharedKernel.Exceptions;
 
 namespace CatalogoArticulosBC.Domain.Exceptions
 {
-    public class MultimediaInvalidaException : Exception
+    public class MultimediaInvalidaException : DomainException
     {
-        public MultimediaInvalidaException(string mensaje) : base(mensaje) { }
+        public MultimediaInvalidaException(string mensaje)
+            : base(
+                message: mensaje,
+                code: "MultimediaInvalida",
+                innerException: null)
+        { }
     }
 }
